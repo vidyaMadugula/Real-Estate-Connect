36d7f78c68ca76c08bc419e5fe4b3d37a5f00f92
vidyaMadugula <vidyamadugula692@gmail.com>
2024-10-14 19:06:45 +0530

    made changes

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,8 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://real-estate-connect.vercel.app/",
+    // origin: "http://localhost:5173",
   },
 });
 
