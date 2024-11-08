5c576983b27cb86467411dfb519dc1bfa890ec7e
vidyaMadugula <124115118+vidyaMadugula@users.noreply.github.com>
2024-11-08 16:42:05 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -34,7 +34,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://real-estate-connect-socket.onrender.com"));
   }, []);
 
   useEffect(() => {
