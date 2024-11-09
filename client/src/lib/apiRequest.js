72fec2eebd8423c204b175572b782486f4da97b4
vidyaMadugula <124115118+vidyaMadugula@users.noreply.github.com>
2024-11-09 20:32:37 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -24,34 +24,34 @@ const apiRequest = axios.create({
   withCredentials: true,
 });
 
-// Add request interceptor for logging and setting Authorization header
-apiRequest.interceptors.request.use((config) => {
-  console.log("Sending request to:", config.url);
-  console.log("With credentials:", config.withCredentials);
+// // Add request interceptor for logging and setting Authorization header
+// apiRequest.interceptors.request.use((config) => {
+//   console.log("Sending request to:", config.url);
+//   console.log("With credentials:", config.withCredentials);
 
-  // Retrieve the token from localStorage
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-    console.log("Authorization header set:", config.headers.Authorization);
-  } else {
-    console.log("No token found in localStorage");
-  }
+//   // Retrieve the token from localStorage
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//     console.log("Authorization header set:", config.headers.Authorization);
+//   } else {
+//     console.log("No token found in localStorage");
+//   }
 
-  return config;
-}, (error) => {
-  console.error("Request error:", error);
-  return Promise.reject(error);
-});
+//   return config;
+// }, (error) => {
+//   console.error("Request error:", error);
+//   return Promise.reject(error);
+// });
 
-// Add response interceptor for debugging responses
-apiRequest.interceptors.response.use((response) => {
-  console.log("Response received from:", response.config.url);
-  console.log("Response status:", response.status);
-  return response;
-}, (error) => {
-  console.error("Response error:", error.response?.status);
-  return Promise.reject(error);
-});
+// // Add response interceptor for debugging responses
+// apiRequest.interceptors.response.use((response) => {
+//   console.log("Response received from:", response.config.url);
+//   console.log("Response status:", response.status);
+//   return response;
+// }, (error) => {
+//   console.error("Response error:", error.response?.status);
+//   return Promise.reject(error);
+// });
 
 export default apiRequest;
