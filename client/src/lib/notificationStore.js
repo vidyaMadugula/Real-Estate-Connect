5b9ff0dccc5c55befc150cf9e590dc1aaa4cefe8
vidyaMadugula <124115118+vidyaMadugula@users.noreply.github.com>
2024-11-10 13:31:49 +0530

    Update notificationStore.js

--- client/src/lib/notificationStore.js
+++ client/src/lib/notificationStore.js
@@ -24,7 +24,7 @@ import apiRequest from "./apiRequest";
 export const useNotificationStore = create((set) => ({
   number: 0,
   fetch: async () => {
-    const res = await apiRequest("/users/notification", { withCredentials: true });
+    const res = await apiRequest("/users/notification");
     set({ number: res.data });
   },
   decrease: () => {
