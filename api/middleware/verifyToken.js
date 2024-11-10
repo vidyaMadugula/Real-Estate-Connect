507f7c79bddd49aa37a7288bf9f008caf75de1a7
vidyaMadugula <124115118+vidyaMadugula@users.noreply.github.com>
2024-11-10 13:11:21 +0530

    Update verifyToken.js

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,14 +1,28 @@
 
+// export const verifyToken = (req, res, next) => {
+//   console.log("Hello");
+//   const token = req.cookies.token;
+//   console.log("Token received:", token);
+
+//   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+
+//   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
+//     if (err) return res.status(403).json({ message: "Token is not Valid!" });
+//     req.userId = payload.id;
+//     next();
+//   });
+// };
+import jwt from "jsonwebtoken";
+
 export const verifyToken = (req, res, next) => {
-  console.log("Hello");
   const token = req.cookies.token;
-  console.log("Token received:", token);
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
     if (err) return res.status(403).json({ message: "Token is not Valid!" });
     req.userId = payload.id;
+
     next();
   });
 };
