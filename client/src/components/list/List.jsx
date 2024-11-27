7b794048f7b003748f84777105a1324c84819b04
vidyaMadugula <124115118+vidyaMadugula@users.noreply.github.com>
2024-11-27 14:10:16 +0530

    Update List.jsx

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,15 +1,33 @@
-import './list.scss'
-import Card from"../card/Card"
+// import './list.scss'
+// import Card from"../card/Card"
 
 
-function List({posts}){
+// function List({posts}){
+//   return (
+//     <div className='list'>
+//       {posts.map(item=>(
+//         <Card key={item.id} item={item}/>
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default List
+import './list.scss';
+import Card from "../card/Card";
+
+function List({ posts }) {
   return (
     <div className='list'>
-      {posts.map(item=>(
-        <Card key={item.id} item={item}/>
-      ))}
+      {posts && posts.length > 0 ? (
+        posts.map(item => (
+          <Card key={item.id} item={item} />
+        ))
+      ) : (
+        <p>No posts available.</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
