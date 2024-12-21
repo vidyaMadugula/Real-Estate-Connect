1d9dfa02fe5dfb60797f8b994646b0bac0f0d0bc
vidyaMadugula <vidyamadugula692@gmail.com>
2024-12-21 18:50:55 +0530

    made changes

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,36 +1,15 @@
 
-// import jwt from "jsonwebtoken";
-
-// export const verifyToken = (req, res, next) => {
-//   const token = req.cookies.token;
-//   // console.log("Token received:", token);
-
-//   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
-  
-
-//   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-//     if (err) return res.status(403).json({ message: "Token is not Valid!" });
-//     req.userId = payload.id;
-
-//     next();
-//   });
-// };
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  // Retrieve the token from cookies or Authorization header
-  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
+  // console.log("Token received:", token);
 
-  if (!token) {
-    return res.status(401).json({ message: "Not Authenticated!" });
-  }
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+  
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
-    if (err) {
-      return res.status(403).json({ message: "Token is not Valid!" });
-    }
-
-    // Attach user ID to the request object
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is not Valid!" });
     req.userId = payload.id;
 
     next();
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,33 +1,11 @@
 
 
-// import axios from "axios";
-
-// const apiRequest = axios.create({
-//   //  baseURL: import.meta.env.VITE_API_URL,
-//    baseURL: "http://localhost:8800/api",
-//   withCredentials: true,
-// });
-
-// export default apiRequest;
-
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  // baseURL: "http://localhost:8800/api",
-  withCredentials: true, // Ensure cookies are included in requests
+   baseURL: import.meta.env.VITE_API_URL,
+  //  baseURL: "http://localhost:8800/api",
+  withCredentials: true,
 });
-
-// Add a request interceptor to attach the Authorization header
-apiRequest.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token"); // Retrieve the token from localStorage
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`; // Add the token to the Authorization header
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default apiRequest;
